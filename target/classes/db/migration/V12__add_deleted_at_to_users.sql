d77d1845ee59d495de61d9e5fff1d80f21b2c9f5
khangnt <khangnt@oneempower.com>
2026-05-31 16:08:59 +0700

    chore: add .gitignore and remove build artifacts from tracking

--- target/classes/db/migration/V12__add_deleted_at_to_users.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE users
-ADD COLUMN deleted_at TIMESTAMP;
-
-CREATE INDEX idx_users_deleted_at ON users (deleted_at);
-
--- target/classes/db/migration/V14__update_role_default_to_users.sql
+++
@@ -1,4 +0,0 @@
--- Cập nhật role của tất cả người dùng đang là TEACHER thành ADMIN
-UPDATE users
-SET role = 'ADMIN'::user_role
-WHERE role = 'TEACHER'::user_role;
